1d58243720b4d5b8eab18e0fcb97e484616f5dca
Mirko Sinibaldi <sinibaldimirk88@gmail.com>
2019-07-18 09:43:18 +0200

    Models

---
+++ src/main/java/it/k2/demo/repositories/BookRepository.java
@@ -0,0 +1,19 @@
+package it.k2.demo.repositories;
+
+import it.k2.demo.models.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+
+public interface BookRepository extends JpaRepository<Book, Integer>
+{
+    List<Book> findAllById_Author(Integer id_author);
+    List<Book> findAllById_Genre(Integer id_genre);
+    List<Book> findAllById_Publisher(Integer id_publisher);
+    Book findByTitle (String title);
+    List<Book> findById_Book(Integer id);
+    List<Book> findAllBooks();
+}
