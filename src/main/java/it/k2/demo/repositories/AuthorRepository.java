fc6646807f5d7d51ca4d3a97d31a51d00ff86a32
Mirko Sinibaldi <sinibaldimirk88@gmail.com>
2019-07-18 09:48:35 +0200

    Models

---
+++ src/main/java/it/k2/demo/repositories/AuthorRepository.java
@@ -0,0 +1,16 @@
+package it.k2.demo.repositories;
+
+import it.k2.demo.models.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AuthorRepository extends JpaRepository<Author, Integer>
+{
+    Author findByName(String name);
+    Author findById_Author(Integer id);
+    Author findById_Book(Integer id_book);
+    List<Author> findAll();
+}
