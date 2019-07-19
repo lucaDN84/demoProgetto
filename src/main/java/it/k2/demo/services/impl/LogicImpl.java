27d3477f2d15aa4c7b018462532153fc60db67d1
lucaDN84 <lucadelnoce84@gmail.com>
2019-07-19 14:04:33 +0200

    nuova classe business

--- src/main/java/it/k2/demo/services/impl/LogicImpl.java
+++ src/main/java/it/k2/demo/services/impl/LogicImpl.java
@@ -15,12 +15,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 @Service
+@Transactional
 public class LogicImpl implements Logic {
 
     @Autowired
